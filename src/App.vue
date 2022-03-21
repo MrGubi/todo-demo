0297d64e0e9749b58600abce5b9ee9f33e10dbbf
Gubi <O.Roman@web.de>
2022-03-21 21:26:11 +0100

    Container/Task

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+  <TodoContainer/>
+  </div>
+</template>
+
+<script>
+import TodoContainer from './components/TodoContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoContainer
+  }
+}
+</script>
+
+<style>
+body {
+  background: rgb(44, 44, 44);
+}
+#app {
+  display: flex;
+  text-align: center;
+}
+</style>
---
+++ src/components/TodoContainer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>My DoTo List</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoContainer',
+    data() {
+        return {
+            tasks: [
+                {
+                    id: 1,
+                    title: "This tasks is IMPORTANT",
+                    priority: 5,
+                    deadline: '23.03.2022',
+                    completed: false,
+                },
+                {
+                    id: 2,
+                    title: "This tasks is less important",
+                    priority: 3,
+                    deadline: '23.03.2023',
+                    completed: false,
+                },
+            ]  
+        }
+    }
+}
+</script>
